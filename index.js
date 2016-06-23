c5f443f026072cac9671dbe0f0539a7b0cdd721b
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-06-23 15:13:16 +1200

    testing

--- index.js
+++ index.js
@@ -1,25 +1,97 @@
-var express = require('express')
-app = express()
-var dotenv = require('dotenv')
-dotenv.load()
+var Discogs = require('disconnect').Client;
+
+// var db = new Discogs().database();
+// db.getRelease(176126, function(err, data){
+//     console.log(data);
+// });
+
+
+var a,b,c
+
+
+
+// // var express = require('express')  //Not pushing to browser yet
+// // app = express()
+// // var agent = require('superagent-promise')(require('superagent'), Promise)
+// var dotenv = require('dotenv')
+// dotenv.load()
 var request = require('superagent')
-
-
-// Set view engine?
-app.listen(3000, () => {
-	console.log("----Listenting on port 3000----")
-
-})
-app.get('/', function(req, res) {
-	res.send('hello wffforld!')
-})
-
-var searchTerm = 'Kanye'
-var musicAPI = 'http://api.musixmatch.com/ws/1.1/track.search?apikey=' + process.env.MUSIC_MATCH_KEY + '&q_artist=queen'
+// var getArtistId = require('./lib/get-artist-id')
+// var getArtistSongs = require('./lib/get-artist-songs')
+// var Promise = require('promise')
 
 request
-	.get(musicAPI)
-	.end((err, res) => {
-		console.log(err)
-		console.log(res)
+	.get('http://api.fixer.io/latest?base=USD&symbols=GBP')
+	.end(function(err, res) {
+		resText = JSON.parse(res.text)
+		console.log(resText.rates.GBP)
+		a = resText.rates.GBP
+
+		request
+	.get('http://api.fixer.io/latest?base=GBP&symbols=NZD')
+	.end(function(err, res) {
+		resText = JSON.parse(res.text)
+		console.log(resText.rates.NZD)
+		b = resText.rates.NZD
+
+		request
+	.get('http://api.fixer.io/latest?base=NZD&symbols=USD')
+	.end(function(err, res) {
+		resText = JSON.parse(res.text)
+		console.log(resText.rates.USD)
+		c = resText.rates.USD
+		console.log("AAA", a,b,c, a*b*c)
 	})
+
+
+
+	})
+
+
+
+
+
+
+	})
+// request
+// 	.get('http://api.fixer.io/latest?base=GBP&symbols=NZD')
+// 	.end(function(err, res) {
+// 		resText = JSON.parse(res.text)
+// 		console.log(resText.rates.NZD)
+// 		b = resText.rates.NZD
+// 	})
+// request
+// 	.get('http://api.fixer.io/latest?base=NZD&symbols=USD')
+// 	.end(function(err, res) {
+// 		resText = JSON.parse(res.text)
+// 		console.log(resText.rates.USD)
+// 		c = resText.rates.USD
+// 	})
+
+	// console.log(a,b,c,a*b*c)
+
+// var artistName = 'kanye'
+// var musicAPI = 'http://api.musixmatch.com/ws/1.1/artist.search?q_artist='+artistName+'&page_size10&apikey=' + process.env.MUSIC_MATCH_KEY
+
+
+// var promise = new Promise(function (resolve, reject) {
+//   request.get(musicAPI, function (err, res) {
+//     if (err) reject(err);
+//     else resolve(res);
+//   });
+// });
+
+// promise
+// 	.then(getArtistId)
+// 	// Get list of artist songs
+// 	.then(function(artistId) {
+// 		console.log(artistName, artistId)
+// 		return artistId
+// 	})
+// 	.then(getArtistSongs)
+// 	.catch(function(err) {
+// 		console.log("Promise error! It is-----: ", err)
+// 	})
+
+
+
